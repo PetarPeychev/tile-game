d41aedc0d1bb50e11599a3ea26456f062f1108be
PetarPeychev <petarpeychev98@gmail.com>
2018-11-08 01:01:02 +0000

    limited camera to edges of map

--- game/camera.py
+++ game/camera.py
@@ -7,9 +7,15 @@ class Camera:
         self.map_height = map_height
 
     def apply(self, entity):
-        return entity.rect.move(self.camera.topLeft)
+        return entity.rect.move(self.camera.topleft)
 
     def update(self, target):
         x = -target.rect.x + int(settings.DISPLAY_WIDTH / 2)
         y = -target.rect.y + int(settings.DISPLAY_HEIGHT / 2)
         self.camera = pygame.Rect(x, y, self.map_width, self.map_height)
+
+        # limit camera to edges of the map
+        x = min(0, x)
+        y = min(0, y)
+        x = max(-(self.map_width - settings.DISPLAY_WIDTH), x)
+        y = max(-(self.map_height - settings.DISPLAY_HEIGHT), y)
