86447352fa7d9a10773add0a821a006026bac22b
PetarPeychev <petarpeychev98@gmail.com>
2018-11-29 20:32:31 +0000

    removed silly recursion function

--- game/misc_functions.py
+++
@@ -1,9 +0,0 @@
-from sys import getrecursionlimit, setrecursionlimit
-from contextlib import contextmanager
-
-@contextmanager
-def recursionlimit(n=1000):
-	rec_limit = getrecursionlimit()
-	setrecursionlimit(n)
-	yield
-	setrecursionlimit(rec_limit)
